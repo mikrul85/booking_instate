c77b05958b219e7e3e7acba631914da486b9a578
Parabeac-Nest <admin@parabeac.com>
2022-10-27 22:12:24 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/booking_instate_colors.g.dart
@@ -0,0 +1,86 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class BookingInstateColors {
+
+static const Color white = Color(0xffffffff);
+
+static const Color primaryText = Color(0xff2d3748);
+
+static const Color success = Color(0xff4bb46f);
+
+static const Color successDark = Color(0xff2e605b);
+
+static const Color amber = Color(0xfff57b02);
+
+static const Color amberLight = Color(0xffeeab6b);
+
+static const Color danger = Color(0xffee7168);
+
+static const Color warning = Color(0xffffc107);
+
+static const Color info = Color(0xff2085d4);
+
+static const Color textSecond = Color(0xff75a0ae);
+
+static const Color helpingText = Color(0xff999999);
+
+static const Color textPrimary = Color(0xffb0ddeb);
+
+static const Color main1Light = Color(0xffc2d169);
+
+static const Color main2Light = Color(0xffa1a1e5);
+
+static const Color main3Light = Color(0xff4c9cd3);
+
+static const Color main2 = Color(0xff837ce6);
+
+static const Color main3 = Color(0xff3764f6);
+
+static const Color main1 = Color(0xffa9d61d);
+
+static const Color bGSecond = Color(0xff213c53);
+
+static const Color bGElements = Color(0xff385065);
+
+static const Color bGDIsabled = Color(0xff212e3c);
+
+static const Color stroke = Color(0xff4f728c);
+
+static const Color bGMain = Color(0xff05263f);
+
+static const Color black = Color(0xff000000);
+
+static const Color grey3(secText) = Color(0xff828282);
+
+static const Color grey300 = Color(0xffa1b0cc);
+
+static const Color lightColor = Color(0xffffffff);
+
+static const Color textColor = Color(0xff000000);
+
+static const Color grey600 = Color(0xffa0aec0);
+
+static const Color secondaryText = Color(0xff656565);
+
+static const Color basecolorWhite = Color(0xffffffff);
+
+static const Color blue = Color(0xff009aff);
+
+static const Color gray2 = Color(0xff4f4f4f);
+
+static const Color orange = Color(0xfff2994a);
+
+static const Color green3 = Color(0xff6fcf97);
+
+static const Color green1 = Color(0xff219653);
+
+static const Color gray3 = Color(0xff828282);
+
+static const Color grayColor = Color(0xff5f5f5f);
+}
---
+++ lib/theme/booking_instate_text_styles.g.dart
@@ -0,0 +1,190 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class BookingInstateTextStyles {
+
+static const TextStyle header = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Open Sans',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle text = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Open Sans',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle text12 = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Open Sans',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle lib,H1 = TextStyle(
+  fontSize: 36,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 0,
+  fontFamily: 'Open Sans',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle text,Checkbox = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Open Sans',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle help = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Open Sans',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle text,Btn,Big = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Open Sans',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle text,Btn,Small = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Open Sans',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle lib,Text = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Open Sans',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.italic,
+)
+;
+
+static const TextStyle 14Sp•BUTTON = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 0,
+  fontFamily: 'Roboto',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle label = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Open Sans',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle default = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Open Sans',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle dropdownText = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Open Sans',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle 14Sp•Body2Medium = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 0,
+  fontFamily: 'Roboto',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle 16Sp•SubheadingRegular = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Roboto',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle 12Sp•CaptionRegular = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Roboto',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle logo = TextStyle(
+  fontSize: 28,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Open Sans',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle 20Sp•TitleMedium = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 0,
+  fontFamily: 'Roboto',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+}
